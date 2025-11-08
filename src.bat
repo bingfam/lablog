1811e3a02c52c64cb78ea8b443b44ae4225f0bf9
bingfam <bingfam@outlook.kr>
2025-11-08 22:41:51 +0900

    update

--- src.bat
+++ src.bat
@@ -1,5 +1,5 @@
 REM 파일 복사
-robocopy ..\obsidian-vault\ content\ /xd .obsidian .git template /mir
+robocopy ..\obsidian-vault\ content\ /xd .obsidian .git template private /mir
 
 REM github에 파일 올리기
 npx quartz sync
