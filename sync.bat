ffd1ccbe251b55d2f0369748a78d1ab3ccc6f587
bingfam <bingfam@outlook.kr>
2025-11-09 18:18:44 +0900

    Quartz sync: Nov 9, 2025, 6:18 PM

--- sync.bat
+++ sync.bat
@@ -1,8 +1,3 @@
-REM 파일 복사
-
-robocopy "..\obsidian-bingfam\" "content\" /xd .obsidian .git template private /mir
-
-
-REM github에 파일 올리기
+robocopy ..\obsidian-bingfam\ content\ /xd .obsidian .git template private /mir
 
 npx quartz sync
